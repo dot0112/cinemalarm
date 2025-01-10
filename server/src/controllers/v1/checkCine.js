897eabfcf357260765da5a8a61aed7c2621a88e7
DESKTOP-6B7URQN\deu <dot0112@naver.com>
2025-01-11 00:02:28 +0900

    # date 컨트롤러 작성 완료 * 요청의 body에서 'multiplex'를 추출하여 서비스로 전달 * 컨트롤러 오류에 대하여 client 전송을 추가 * 라우터와 컨트롤러 함수 연결

--- server/src/controllers/v1/checkCine.js
+++ server/src/controllers/v1/checkCine.js
@@ -1,12 +1,31 @@
 const statService = require("../../services/v1/checkCine/stat");
+const dateService = require("../../services/v1/checkCine/date");
 
 const stat = async (req, res) => {
     try {
-        const results = await statService.getStatus();
-        res.json(results);
+        const result = await statService.getStatus();
+        res.json(result);
     } catch (err) {
         global.errorLogger(err, req);
+        res.status(500).json({
+            error: "An error occurred on the server.",
+            message: err.message || "Unknown error",
+        });
     }
 };
 
-module.exports = { stat };
+const date = async (req, res) => {
+    try {
+        const mode = req.body.multiplex;
+        const result = await dateService.getDates(mode);
+        res.json(result);
+    } catch (err) {
+        global.errorLogger(err, req);
+        res.status(500).json({
+            error: "An error occurred on the server.",
+            message: err.message || "Unknown error",
+        });
+    }
+};
+
+module.exports = { stat, date };
