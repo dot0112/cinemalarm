4e0561be4b8ee949da0b9e9f9fc5523c610e1c98
DESKTOP-6B7URQN\deu <dot0112@naver.com>
2025-01-21 23:52:11 +0900

    # checkCine 테스트 내용 수정 * checkCine 반환 dto 추가에 따른 테스트 예측 결과 수정

--- server/tests/integration/checkCine/getMovies.test.js
+++ server/tests/integration/checkCine/getMovies.test.js
@@ -15,7 +15,11 @@ describe("getMovies 테스트", () => {
             date: "1970-01-01",
             cinema: "test",
         });
-        expect(result).toEqual({ movie: [] });
+        expect(result.status).toBe(200);
+        expect(result.data.mode).toBe("C");
+        expect(result.data.date).toEqual("1970-01-01");
+        expect(result.data.cinema).toEqual("test");
+        expect(result.data.result).toEqual({ movie: [] });
     });
 
     test("LOTTE CINEMA의 선택 가능 영화를 확인한다", async () => {
@@ -35,7 +39,12 @@ describe("getMovies 테스트", () => {
             date: "1970-01-01",
             cinema: "test|test|test",
         });
-        expect(result).toEqual({ movie: ["test1", "test2"] });
+
+        expect(result.status).toBe(200);
+        expect(result.data.mode).toBe("L");
+        expect(result.data.date).toEqual("1970-01-01");
+        expect(result.data.cinema).toEqual("test|test|test");
+        expect(result.data.result).toEqual({ movie: ["test1", "test2"] });
     });
 
     test("MEGABOX의 선택 가능 영화를 확인한다", async () => {
@@ -57,75 +66,120 @@ describe("getMovies 테스트", () => {
             date: "1970-01-01",
             cinema: "test/test",
         });
-        expect(result).toEqual({ movie: ["test1", "test3"] });
+        expect(result.status).toBe(200);
+        expect(result.data.mode).toBe("M");
+        expect(result.data.date).toEqual("1970-01-01");
+        expect(result.data.cinema).toEqual("test/test");
+        expect(result.data.result).toEqual({ movie: ["test1", "test3"] });
     });
 
-    test("잘못된 요청에 대해 빈 배열을 반환한다 - Multiplex 기호", async () => {
-        const result = await getMovies({
-            mode: "Invalid mode",
-            date: "1970-01-01",
-            cinema: "test",
-        });
-        expect(result).toEqual({ movie: [] });
+    test("잘못된 요청에 대해 오류를 발생시킨다 - Multiplex 기호", async () => {
+        try {
+            await getMovies({
+                mode: "Invalid mode",
+                date: "1970-01-01",
+                cinema: "test",
+            });
+        } catch (err) {
+            expect(err.status).toBe(400);
+            expect(err.message).toEqual("Invalid mode: Invalid mode");
+        }
     });
 
-    test("잘못된 요청에 대해 빈 배열을 반환한다 - 날짜 형식", async () => {
-        const resultC = await getMovies({
-            mode: "C",
-            date: "19700101",
-            cinema: "test",
-        });
-        const resultL = await getMovies({
-            mode: "L",
-            date: "19700101",
-            cinema: "test|test|test",
-        });
-        const resultM = await getMovies({
-            mode: "M",
-            date: "19700101",
-            cinema: "test/test",
-        });
-        expect(resultC).toEqual({ movie: [] });
-        expect(resultL).toEqual({ movie: [] });
-        expect(resultM).toEqual({ movie: [] });
+    test("잘못된 요청에 대해 오류를 발생시킨다 - 날짜 형식", async () => {
+        try {
+            await getMovies({
+                mode: "C",
+                date: "19700101",
+                cinema: "test",
+            });
+        } catch (err) {
+            expect(err.status).toBe(400);
+            expect(err.message).toBe(
+                "Invalid date format. Expected format: YYYY-MM-DD, Received data: 19700101"
+            );
+        }
+        try {
+            await getMovies({
+                mode: "L",
+                date: "19700101",
+                cinema: "test|test|test",
+            });
+        } catch (err) {
+            expect(err.status).toBe(400);
+            expect(err.message).toBe(
+                "Invalid date format. Expected format: YYYY-MM-DD, Received data: 19700101"
+            );
+        }
+        try {
+            await getMovies({
+                mode: "M",
+                date: "19700101",
+                cinema: "test/test",
+            });
+        } catch (err) {
+            expect(err.status).toBe(400);
+            expect(err.message).toBe(
+                "Invalid date format. Expected format: YYYY-MM-DD, Received data: 19700101"
+            );
+        }
     });
 
-    test("잘못된 요청에 대해 빈 배열을 반환한다 - 지점 형식", async () => {
+    test("잘못된 요청에 대해 오류를 발생시킨다 - 지점 형식", async () => {
         // const resultC = await getMovies("C", "1970-01-01", ""); - 미구현
-        const resultL = await getMovies({
-            mode: "L",
-            date: "1970-01-01",
-            cinema: "",
-        });
-        const resultM = await getMovies({
-            mode: "M",
-            date: "1970-01-01",
-            cinema: "",
-        });
-        // expect(resultC).toEqual({ movie: [] });
-        expect(resultL).toEqual({ movie: [] });
-        expect(resultM).toEqual({ movie: [] });
+        try {
+            await getMovies({
+                mode: "L",
+                date: "1970-01-01",
+                cinema: "",
+            });
+        } catch (err) {
+            expect(err.status).toBe(400);
+            expect(err.message).toBe("Invalid cinema ID: ");
+        }
+        try {
+            await getMovies({
+                mode: "M",
+                date: "1970-01-01",
+                cinema: "",
+            });
+        } catch (err) {
+            expect(err.status).toBe(400);
+            expect(err.message).toBe("Invalid cinema ID: ");
+        }
     });
 
-    test("잘못된 반환에 대해 빈 배열을 반환한다", async () => {
+    test("잘못된 반환에 대해 오류를 발생시킨다", async () => {
         axios.post.mockResolvedValue({ status: 500 });
-        const resultC = await getMovies({
-            mode: "C",
-            date: "1970-01-01",
-            cinema: "",
-        });
-        const resultL = await getMovies({
-            mode: "L",
-            date: "1970-01-01",
-            cinema: "test|test|test",
-        });
-        const resultM = await getMovies({
-            mode: "M",
-            date: "1970-01-01",
-            cinema: "test/test",
-        });
-        expect(resultC).toEqual({ movie: [] });
-        expect(resultL).toEqual({ movie: [] });
-        expect(resultM).toEqual({ movie: [] });
+        try {
+            await getMovies({
+                mode: "C",
+                date: "1970-01-01",
+                cinema: "test",
+            });
+        } catch (err) {
+            expect(err.status).toBe(400);
+            expect(err.message).toBe("Failed to retrieve movie data");
+        }
+        try {
+            await getMovies({
+                mode: "L",
+                date: "1970-01-01",
+                cinema: "test|test|test",
+            });
+        } catch (err) {
+            expect(err.status).toBe(400);
+            expect(err.message).toBe("Failed to retrieve movie data");
+        }
+        try {
+            await getMovies({
+                mode: "M",
+                date: "1970-01-01",
+                cinema: "test/test",
+            });
+        } catch (err) {
+            expect(err.status).toBe(400);
+            expect(err.message).toBe("Failed to retrieve movie data");
+        }
     });
 });
--- server/tests/integration/checkCine/getStatus.test.js
+++ server/tests/integration/checkCine/getStatus.test.js
@@ -11,7 +11,6 @@ describe("getStatus 통합 테스트", () => {
     });
 
     test("모든 API의 동작 여부를 확인한다", async () => {
-        // mock 된 axios의 응답
         axios.post
             .mockResolvedValueOnce({ status: 200 })
             .mockResolvedValueOnce({ status: 200 })
@@ -25,8 +24,8 @@ describe("getStatus 통합 테스트", () => {
 
         expect(global.errorLogger).not.toHaveBeenCalled();
 
-        // 결과가 { C: false(구현 전), L: true, M: true }일 것이라 예상
-        expect(result).toEqual({ C: false, L: true, M: true });
+        expect(result.status).toBe(200);
+        expect(result.data.result).toEqual({ C: false, L: true, M: true });
     });
 
     test("실패한 API를 처리한다", async () => {
@@ -39,6 +38,7 @@ describe("getStatus 통합 테스트", () => {
         const result = await getStatus();
 
         // 결과는 { C: false(구현 전), L: false, M: true }
-        expect(result).toEqual({ C: false, L: false, M: true });
+        expect(result.status).toBe(200);
+        expect(result.data.result).toEqual({ C: false, L: false, M: true });
     });
 });
